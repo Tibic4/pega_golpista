9fb2a89171fb090b9e5105f9e75b79518699a319
Alton Jorge <bicagold@gmail.com>
2022-09-05 15:05:50 -0300

    elastic-progress

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,9 +6,28 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
-    # Verify if query is present
-    if params[:query].present?
-      @tasks = Task.advanced_search(params[:query])
+    if params[:search].present? && params[:query].blank? && params[:zip].blank?
+      @tasks = Task.search(params[:search], page: params[:page], per_page: 10)
+
+    elsif params[:search].present? && params[:query].present? && params[:zip].blank?
+      @tasks = Task.search(params[:search].to_s, where: { date: params[:query] }, page: params[:page], per_page: 10)
+
+    elsif params[:search].present? && params[:query].blank? && params[:zip].present?
+      @tasks = Task.search(params[:search].to_s, where: { cep: params[:zip] }, page: params[:page], per_page: 10)
+
+    elsif params[:search].present? && params[:query].present? && params[:zip].present?
+      @tasks = Task.search(params[:search].to_s, where: { date: params[:query], cep: params[:zip] },
+                                                 page: params[:page], per_page: 10)
+
+    elsif params[:query].present? && params[:search].blank?
+      @tasks = Task.where(date: params[:query]).page(params[:page]).per(10).order("created_at DESC")
+
+    elsif params[:zip].present? && params[:search].blank?
+      @tasks = Task.where(cep: params[:zip]).page(params[:page]).per(10).order("created_at DESC")
+
+    elsif params[:search].present? && params[:query].present?
+      # Search by query and sort by date
+
     else
       @tasks = Task.all.page(params[:page]).per(10).order("created_at DESC")
     end
@@ -53,28 +72,23 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1 or /tasks/1.json
   def update
     # Only for admins
     # respond_to do |format|
     #   if @task.update(task_params)
     #     format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @task }
     #   else
     #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @task.errors, status: :unprocessable_entity }
     #   end
     # end
   end
 
-  # DELETE /tasks/1 or /tasks/1.json
   def destroy
     # Only for admins
     # @task.destroy
 
     # respond_to do |format|
     #   format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
-    #   format.json { head :no_content }
     # end
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,35 @@
 class Task < ApplicationRecord
-  searchkick
+  # Searchkick callbacks and mappings
+
   has_many :scammers
   accepts_nested_attributes_for :scammers, allow_destroy: true
-  validates :cep, correios_cep: true
-
-  # Advance search
-  def self.advanced_search(search_results)
-    if search_results
-      Task.search(search_results, fields: %i[cep scam_type date], match: :word_start, misspellings: { below: 5 })
-    else
-      all
-    end
-  end
+  # validates :cep, correios_cep: true
+  searchkick callbacks: :async, word_start: %i[name cep scam_type],
+             merge_mappings: true,
+             settings: {
+               analysis: {
+                 analayzer: {
+                   custom_char: {
+                     type: "custom",
+                     tokenizer: "whitespace",
+                     filter: ["lowercase", "asciifolding"]
+                   }
+                 },
+                 filter: {
+                   custom_filter: {
+                     type: "word_delimiter",
+                     type_table: ['# => ALPHA', '( => ALPHA', ') => ALPHA', ': => ALPHA']
+                   }
+                 }
+               }
+             },
+             mappings: {
+               tasks: {
+                 properties: {
+                   cep: { type: "integer", index: 'analyzed', analyzer: "custom_char" },
+                   scam_type: { type: "string", index: 'analyzed', analyzer: "custom_char" },
+                   date: { type: "date" }
+                 }
+               }
+             }
 end
