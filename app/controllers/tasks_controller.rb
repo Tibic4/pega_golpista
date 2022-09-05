f553ed542b815d835d4ff3d72cb0cb7b7a212ffa
Alton Jorge <bicagold@gmail.com>
2022-09-05 17:30:16 -0300

    Map adjust

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,8 @@ class TasksController < ApplicationController
 
   # GET /tasks or /tasks.json
   def index
+    map
+    # Search
     if params[:search].present? && params[:query].blank? && params[:zip].blank?
       @tasks = Task.search(params[:search], page: params[:page], per_page: 10)
 
@@ -20,7 +22,7 @@ class TasksController < ApplicationController
                                                  page: params[:page], per_page: 10)
 
     elsif params[:query].present? && params[:search].blank? && params[:zip].blank?
-      @tasks = Task.where(date: params[:query]).page(params[:page]).per(10).order("created_at DESC")
+      @tasks = Task.search_data(params[:query], page: params[:page], per_page: 10)
 
     elsif params[:query].present? && params[:search].blank? && params[:zip].present?
       @tasks = Task.where(date: params[:query], cep: params[:zip]).page(params[:page]).per(10).order("created_at DESC")
@@ -99,6 +101,35 @@ class TasksController < ApplicationController
     dddi = JSON.parse(ddds)
     hash = dddi.select { |ddd| ddd["ddd"] }
     @ddd = hash["ddd"].to_i
+  end
+
+  # Scam types for select
+
+  @scam_types = ["whatsapp", "telefone", "email", "facebook", "instagram", "outros"]
+
+  # Render map
+
+  def map
+    @zones = Zone.where.not(latitude: nil, longitude: nil) # returns zones with coordinates
+    # Mark a zone DDD in google maps
+    @markers = @zones.map do |zone|
+      {
+        lat: zone.latitude,
+        lng: zone.longitude,
+        ddd: zone.ddd
+      }
+    end
+    # Count total scamers
+    @total_scammers = Zone.where.not(count_of_scammers: nil).sum(:count_of_scammers)
+
+    # Count the number of scammers in each ddd
+    @countOfScammer = Zone.where.not(count_of_scammers: 0)
+    @countOfScammers = @countOfScammer.map do |zone|
+      {
+        ddd: zone.ddd,
+        count_of_scammers: zone.count_of_scammers
+      }
+    end
   end
 
   private
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,32 +4,5 @@ class Task < ApplicationRecord
   has_many :scammers
   accepts_nested_attributes_for :scammers, allow_destroy: true
   # validates :cep, correios_cep: true
-  searchkick callbacks: :async, word_start: %i[name cep scam_type],
-             merge_mappings: true,
-             settings: {
-               analysis: {
-                 analayzer: {
-                   custom_char: {
-                     type: "custom",
-                     tokenizer: "whitespace",
-                     filter: ["lowercase", "asciifolding"]
-                   }
-                 },
-                 filter: {
-                   custom_filter: {
-                     type: "word_delimiter",
-                     type_table: ['# => ALPHA', '( => ALPHA', ') => ALPHA', ': => ALPHA']
-                   }
-                 }
-               }
-             },
-             mappings: {
-               tasks: {
-                 properties: {
-                   cep: { type: "integer", index: 'analyzed', analyzer: "custom_char" },
-                   scam_type: { type: "string", index: 'analyzed', analyzer: "custom_char" },
-                   date: { type: "date" }
-                 }
-               }
-             }
+  searchkick callbacks: :async, word_start: %i[name cep scam_type]
 end
