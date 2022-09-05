4ccfe43420d86c235a955507eb62014330c14fc3
Alton Jorge <bicagold@gmail.com>
2022-09-05 20:29:05 -0300

    heroku

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,5 @@ class Task < ApplicationRecord
   has_many :scammers
   accepts_nested_attributes_for :scammers, allow_destroy: true
   # validates :cep, correios_cep: true
-  searchkick callbacks: :async, word_start: %i[name cep scam_type]
+  searchkick
 end
