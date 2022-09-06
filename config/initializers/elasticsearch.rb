c40919215f324f086a429e6d588c2d2a38b821f5
Alton Jorge <bicagold@gmail.com>
2022-09-05 21:10:58 -0300

    heroku-new

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,8 @@
-ENV["ELASTICSEARCH_URL] = "https://ypev0wu2qs:7ttn39f9fj@ash-744992710.us-east-1.bonsaisearch.net:443"
+if Rails.env == 'production'
+  url = ENV['BONSAI_URL']
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: url, logs: true})
+else
+  Elasticsearch::Model.client = Elasticsearch::Client.new host: 'localhost:9200'
+end
+
+# ENV["ELASTICSEARCH_URL] = "https://ypev0wu2qs:7ttn39f9fj@ash-744992710.us-east-1.bonsaisearch.net:443"
