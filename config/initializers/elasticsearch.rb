cffb71d84f646fe931170933825ae518b4760ced
Alton Jorge <bicagold@gmail.com>
2022-09-05 21:24:35 -0300

    herokuu deploy

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  url = ENV['BONSAI_URL']
+  url = ENV['https://ypev0wu2qs:7ttn39f9fj@ash-744992710.us-east-1.bonsaisearch.net:443']
   transport_options = {  request: { timeout: 250 } }
   options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
   Searchkick.client = Elasticsearch::Client.new(options)
