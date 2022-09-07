bae32d516592c6bbe66680f76161cea790fe6a52
Alton Vieira <bicagold@gmail.com>
2022-09-07 10:40:50 -0300

    cep adjusts

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,18 +1,24 @@
+require "json"
+require "open-uri"
+
 class Task < ApplicationRecord
-  # Searchkick callbacks and mappings
 
   has_many :scammers
   accepts_nested_attributes_for :scammers, allow_destroy: true
-  validates :cep, presence: true, length: { is: 8 }, numericality: { only_integer: true }, if: :valid_cep?
-
+  validates :cep, presence: true, length: { is: 8 }
+  validate :valid_cep?
   # Scam types for select
-
   SCAM_TYPE = ["Whatsapp", "Site", "Email", "Instagram"]
+
   # Parse url = "https://viacep.com.br/ws/json/" see response is true
   def valid_cep?
     url = "https://viacep.com.br/ws/#{cep}/json/"
-    response = HTTParty.get(url)
-    response["erro"].blank?
+    raise
+    ceps = URI.open(url).read
+    cepis = JSON.parse(ceps)
+    if not cepis["erro"].blank?
+      errors.add(:cep, " inválido")
+    end
   end
 
 end
