cac129ba3741b67e752c295de2d36e2c371bf0b1
Alton Vieira <bicagold@gmail.com>
2022-09-07 09:35:41 -0300

    validate cep

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,9 +3,16 @@ class Task < ApplicationRecord
 
   has_many :scammers
   accepts_nested_attributes_for :scammers, allow_destroy: true
-  # validates :cep, correios_cep: true
+  validates :cep, presence: true, length: { is: 8 }, numericality: { only_integer: true }, if: :valid_cep?
 
   # Scam types for select
 
   SCAM_TYPE = ["Whatsapp", "Site", "Email", "Instagram"]
+  # Parse url = "https://viacep.com.br/ws/json/" see response is true
+  def valid_cep?
+    url = "https://viacep.com.br/ws/#{cep}/json/"
+    response = HTTParty.get(url)
+    response["erro"].blank?
+  end
+
 end
