6bb02c17905467474a03dddb8edb1aef3e8342ba
Alton Vieira <bicagold@gmail.com>
2022-09-12 10:14:54 -0300

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admins
+  # Devise routes only sessions
+  devise_for :admins, only: %i[sessions]
+
   resources :tasks do
     resources :scammers, only: %i[new create show]
   end
   root 'pages#home'
-  get 'pages/reporting'
-  get 'pages/types_of_fraud'
-  get 'pages/prevention'
-  get 'pages/about_us'
-  get 'pages/map'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
